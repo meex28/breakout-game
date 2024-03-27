a5e2358f28f8c283dac999f95a987fc89cfe7f54
Piotr Puchała <260421@student.pwr.edu.pl>
2024-03-27 19:06:13 +0100

    Add camera bounding

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,8 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float damping;
+    [SerializeField] private Vector3 minValue; 
+    [SerializeField] private Vector3 maxValue;
 
     private Vector3 vel = Vector3.zero;
 
@@ -17,7 +19,15 @@ public class CameraFollow : MonoBehaviour
     void Follow()
     {
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref vel, damping);
+
+        // camera bounding
+        Vector3 boundPosition = new (
+            Mathf.Clamp(targetPosition.x, minValue.x, maxValue.x),
+            Mathf.Clamp(targetPosition.y, minValue.y, maxValue.y),
+            Mathf.Clamp(targetPosition.z, minValue.z, maxValue.z)
+        );
+
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, boundPosition, ref vel, damping);
         transform.position = smoothPosition;
     }
 }
